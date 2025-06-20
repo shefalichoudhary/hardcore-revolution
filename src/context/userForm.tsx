ae05b5d066122da145dd2f2f43e108e621de7c41
unknown <choudharyshefali06@gmail.com>
2025-06-20 03:22:32 -0700

    edit

--- src/context/userForm.tsx
+++ src/context/userForm.tsx
@@ -25,7 +25,7 @@ export default function UserForm(user: FormData) {
   const currentDate = new Date().toLocaleDateString();
 
   return (
-    <div className=" max-w-md md:max-w-xl sm:max-w-lg  pb-4 pt-6 md:pt-14 px-2 md:px-8">
+    <div className="flex items-center justify-center max-w-md md:max-w-xl sm:max-w-lg  pb-4 pt-6 md:pt-14 px-2 md:px-8">
       <form
         onSubmit={handleSubmit(onSubmit)}
         autoComplete="off"
